7628067d45001c69a9ce1d040afb9e0abef248c0
Ayhan Metin <ayhanmet1@gmail.com>
2023-05-12 20:36:29 +0200

    basic tests

---
+++ basic-tests.cy.js
@@ -0,0 +1,31 @@
+///<reference types="cypress" />
+
+describe('basic tests', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('title practice', () => {
+    cy.title().should('eq', 'Automation Exercise');
+  });
+
+  it('url practice', () => {
+    cy.url().should('eq', 'https://automationexercise.com/');
+    cy.url().should('include', 'automationexercise.com/');
+  });
+
+  it('location practice', () => {
+    cy.visit('/login');
+    
+    cy.location('pathname').should('eq','/login');
+    cy.location('protocol').should('eq','https:');
+    cy.location('hostname').should('eq','automationexercise.com');
+    cy.location('hostname').should('include','automationexercise');
+  });
+
+  it.only('log practice', () => {
+    cy.log('Cypress test automation practice document');
+    cy.log('be.visible');
+    cy.log('Numbers',['1','2', '3', '4']);
+  });
+});
