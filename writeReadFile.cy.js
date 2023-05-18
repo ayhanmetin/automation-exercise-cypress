ea90af8527eb119266768cf1bc4631ca2558246b
Ayhan Metin <ayhanmet1@gmail.com>
2023-05-18 23:46:41 +0200

    write read file

---
+++ writeReadFile.cy.js
@@ -0,0 +1,14 @@
+///<reference types="cypress" />
+
+describe('Cypress write file', () => {
+  it('write file', () => {
+    cy.writeFile('writeFile/example.txt', 'Cypress is awesome!');
+    cy.writeFile('writeFile/example.txt', ' Make e2e test great again!', {flag:'a+'});
+    //This Cypress command appends ' Make e2e test great again!' to the existing 'example.txt' file, thanks to the {flag:'a+'}
+
+  });
+
+  it('read file', () => {
+    cy.readFile('writeFile/example.txt').should('contain', 'Cypress')
+  });
+});
